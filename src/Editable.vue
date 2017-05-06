d076428771c8d5b44acf77d44ef068b0cc6432c0
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-05-07 01:14:41 +0200

    copy over first draft from codepen
    https://codepen.io/fiedl/pen/KmyaXK
    
    https://trello.com/c/dn2hmyrP/1131-in-place-editor-mit-vuejs-2017-acee3oox

---
+++ src/Editable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class='read' v-on:click="edit" v-if="!editing">
+      {{value}}
+    </div>
+    <div class='edit' v-if="editing" v-on:keyup.enter="save">
+      <input type="text" v-model="value">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['value', 'editing'],
+    methods: {
+      edit() {
+        this.editing = true
+      },
+      save() {
+        this.editing = false
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Editable from './Editable.vue'
+
+Vue.component('editable', Editable)
 
 new Vue({
   el: '#app',
